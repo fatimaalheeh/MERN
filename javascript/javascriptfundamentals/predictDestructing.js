3a34fdfc18c0e0b309151e32a410629b28cc27db
fatimaalheeh <reem00dosary@gmail.com>
2021-07-11 14:05:04 +0300

    predictDestructuring

--- javascript/javascriptfundamentals/predictDestructing.js
+++ javascript/javascriptfundamentals/predictDestructing.js
@@ -1,11 +1,14 @@
-// 1
+// -----------------------1-----------------------
+
 const cars = ['Tesla', 'Mercedes', 'Honda']
 const [ randomCar ] = cars
 const [ ,otherRandomCar ] = cars
 console.log(randomCar)
+//console logs: Tesla
 console.log(otherRandomCar)
-// console logs: Tesla, Mercedes
+// console logs: Mercedes
 
+// -----------------------2-----------------------
 const employee = {
     name: 'Elon',
     age: 47,
@@ -13,10 +16,11 @@ const employee = {
 }
 const { name: otherName } = employee;
 console.log(name);
+// Reference Error , name undefined
 console.log(otherName);
-// shows error since there is no array in objects
+//'Elon'
 
-// 3
+// -----------------------3-----------------------
 const person = {
     name: 'Phil Smith',
     age: 47,
@@ -24,29 +28,27 @@ const person = {
 }
 const password = '12345';
 const { password: hashedPassword } = person;  
-console.log(password);
-console.log(hashedPassword);
-// console log shows: 12345 and undefined/error
+console.log(password);//12345
+console.log(hashedPassword);//unReference Error,defined
 
-// 4
+
+// -----------------------4-----------------------
 const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
-const [,first] = numbers;
-const [,,,second] = numbers;
-const [,,,,,,,,third] = numbers;
-console.log(first == second);
-console.log(first == third);
-// console logs: true, false
+const [,first] = numbers;//2
+const [,,,second] = numbers;//5
+const [,,,,,,,,third] = numbers;//2
+console.log(first == second);//true
+console.log(first == third);//true
 
-// 5
+// -----------------------5-----------------------
 const lastTest = {
     key: 'value',
     secondKey: [1, 5, 1, 8, 3, 3]
 }
-const { key } = lastTest;
-const { secondKey } = lastTest;
-const [ ,willThisWork] = secondKey;
-console.log(key);
-console.log(secondKey);
-console.log(secondKey[0]);
-console.log(willThisWork);
-// console logs: value, [1, 5, 1, 8, 3, 3], 1, 5
+const { key } = lastTest;//=key
+const { secondKey } = lastTest;//=swcondKey
+const [ ,willThisWork] = secondKey;//secondKey[1]: 5
+console.log(key);//'value'
+console.log(secondKey);//[1,5,1,8,3,3]
+console.log(secondKey[0]);//1
+console.log(willThisWork);//5
