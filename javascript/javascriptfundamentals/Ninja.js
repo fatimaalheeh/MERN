b5c5ab8164a8a1159d6015316f3f9e0740f974fb
fatimaalheeh <reem00dosary@gmail.com>
2021-07-12 11:11:36 +0300

    objectmaster

--- javascript/javascriptfundamentals/Ninja.js
+++ javascript/javascriptfundamentals/Ninja.js
@@ -1,7 +1,7 @@
 class Ninja{
-    constructor(name,health){
+    constructor(name){
         this.name=name;
-        this.health=health;
+        this.health=100;
         this.speed=3;
         this.strength=3;
     }
