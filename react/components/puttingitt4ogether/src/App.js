371baad27afdb82f8d29db2c7323190a253f5b85
fatimaalheeh <reem00dosary@gmail.com>
2021-07-13 13:34:30 +0300

    uploadedall

--- react/components/puttingitt4ogether/src/App.js
+++ react/components/puttingitt4ogether/src/App.js
@@ -6,10 +6,10 @@ function App() {
   return (
     <div className="App">
     
-    <Header firstName={"Jane"} lastName={ "Doe" } age="45" hairColor="Black"/>
-    <Header firstName={"John"} lastName={ "Smith" } age="88" hairColor="Brown"/>
-    <Header firstName={"Fillmore"} lastName={ "Millard" } age="50" hairColor="Brown"/>
-    <Header firstName={"Maria"} lastName={ "Smith" } age="62" hairColor="Brown"/>
+    <Header firstName={"Jane"} lastName={ "Doe" } age={45} hairColor={"Black"}/>
+    <Header firstName={"John"} lastName={ "Smith" } age={88} hairColor={"Brown"}/>
+    <Header firstName={"Fillmore"} lastName={ "Millard" } age={50} hairColor={"Brown"}/>
+    <Header firstName={"Maria"} lastName={ "Smith" } age={62} hairColor={"Brown"}/>
     
     
   </div>
--- react/components/puttingitt4ogether/src/components/header.js
+++ react/components/puttingitt4ogether/src/components/header.js
@@ -2,31 +2,24 @@ import React, { Component } from 'react';
 
 
 class Header extends React.Component {
-
+    
     constructor(props) {
         super(props);
         this.state = {
-            age: 22
+            age: this.props.age,
         };
     }
     increaseAge = () => {
-        
         this.setState({ age: this.state.age+1 });
-    
     }    
- 
     render() {
-            const { firstName, lastName, hairColor } = this.props;
-        
+            const { firstName, lastName, age, hairColor } = this.props;
             return (
                 <div>
                     <h1>{ lastName }, { firstName}</h1>
                     <p>Age: {this.state.age}</p>
                     <p>Hair Color: {hairColor}</p>
-
                         <button onClick={this.increaseAge}>Birthday Button for {firstName} {lastName}</button>
-
-
                 </div>
             );
         }
